edbb9d7eab35ee04ee867a87ae08cd0aaa329195
Tejas Kanikdaley <TejasK@talentica-all.com>
2020-02-21 13:52:59 +0530

    added field validators

--- application.py
+++ application.py
@@ -10,6 +10,8 @@ app.secret_key  = "secret"
 @app.route("/", methods = ["GET","POST"])
 def index():
     reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Success"
     return render_template("index.html",form=reg_form)
 
 if __name__=="__main__":
--- wtforms_fields.py
+++ wtforms_fields.py
@@ -1,9 +1,10 @@
 from flask_wtf  import FlaskForm
-from wtforms import StringField,PasswordField
+from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import InputRequired,Length,EqualTo
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('username_label',validators=[InputRequired(message='Username Required'),Length(min=1,max=20,message="Username must be between 1 to 20 chars")])
-    password = PasswordField('pwd_field')
-    confirm_password = PasswordField('conf_pwd_label')
+    password = PasswordField('pwd_field',validators=[InputRequired(message='Password Required'),Length(min=1,max=10,message="Password must be between 1 to 10 chars")])
+    confirm_password = PasswordField('conf_pwd_label',validators=[InputRequired(message='Confirm Password'),EqualTo('password',message="Passwords Must match")])
+    submit_button = SubmitField('Create')
